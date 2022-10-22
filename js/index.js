cbdfc64c053a5406e66e3610a61f0a3321201ad1
Евгений Степанишин <stepanishin92@mail.ru>
2022-10-22 17:09:24 +0200

    v_0.5

--- js/index.js
+++ js/index.js
@@ -24,3 +24,10 @@ question.forEach((qsitem) => {
   });
 });
 // ////////////////////////////////////////////////////////////////////////////////////////
+// js code for burger menu to add style overflow hidden to body
+let burger = document.querySelectorAll(".hamburger");
+const toggleOverflow = () => {
+  let myBody = document.getElementsByTagName('body')[0]
+  myBody.classList.toggle('ovh')
+}
+// ////////////////////////////////////////////////////////////
