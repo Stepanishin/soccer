e7e96da8e66980a8a94922b0925dd506cb8707e9
Евгений Степанишин <stepanishin92@mail.ru>
2022-10-25 15:47:17 +0200

    v+0.7

--- js/index.js
+++ js/index.js
@@ -27,13 +27,13 @@ question.forEach((qsitem) => {
 // js code for burger menu to add style overflow hidden to body
 let burger = document.querySelectorAll(".hamburger");
 const toggleOverflow = () => {
-  let myBody = document.getElementsByTagName('body')[0]
-  myBody.classList.toggle('ovh')
+  // let myBody = document.getElementsByTagName('body')[0]
+  // myBody.classList.toggle('ovh')
 }
 // ////////////////////////////////////////////////////////////
 // Closing of Hamburger
 const deleteBurgerMenu = () => {
-  let myBody = document.getElementsByTagName('body')[0]
+  // let myBody = document.getElementsByTagName('body')[0]
   toggle.checked = false
-  myBody.classList.remove('ovh')
+  // myBody.classList.remove('ovh')
 }
